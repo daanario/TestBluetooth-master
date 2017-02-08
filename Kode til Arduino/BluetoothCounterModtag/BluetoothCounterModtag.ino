e292910def3953997a85e692d88af5b0fb07b098
daanario <daanario@gmail.com>
2017-02-08 15:19:13 +0100

    dø mig

--- Kode til Arduino/BluetoothCounterModtag/BluetoothCounterModtag.ino
+++ Kode til Arduino/BluetoothCounterModtag/BluetoothCounterModtag.ino
@@ -1,19 +1,12 @@
-/*
- * Test af bluetooth
- * af Lone Østerlund
- */
-
 #include <SoftwareSerial.h>
-int bluetoothTx = 3;
-int bluetoothRx = 4;
+int bluetoothTx = 4;
+int bluetoothRx = 5;
 
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);//Arduino RX,Tx
 char msg;
 int counter = 0;
 void setup(){
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
   bluetooth.begin(115200);//standard bluetooth mate
 }
 
@@ -23,26 +16,64 @@ void loop(){
   Serial.println(counter);
   bluetooth.println(counter);
   counter++;
-  delay(1000);
+  delay(10);
 }
 
-/*
- *Denne funktion modtager på
- * SoftwarerSeriel porten
- * navngivet bluetooth.
- * modtages 't' tændes diode på ben 13
- * modtages 's' slukkes dioden
- */
 void modtagFraBluetooth(){
   char msg;
   if(bluetooth.available()){ 
     msg= (char)bluetooth.read(); 
-    if(msg=='t')
-      digitalWrite(13,HIGH);
-     else if(msg =='s'){
-      digitalWrite(13,LOW);  
+    if(msg=='Forward') {
+      forward();
+    }
+    else if(msg =='Back'){
+      back();
     }      
+
+    else if (msg =='Right') {
+      right();  
+    }
+
+    else if (msg =='Left') {
+      left();
+    }
+
+    
+    
+    
     bluetooth.print(msg); //returner det modtagne
   }
+}
+
+void forward(){
+  digitalWrite(input1,HIGH);
+  digitalWrite(input2,LOW);
+  digitalWrite(input3,HIGH);
+  digitalWrite(input4,LOW);
+  
+}
+
+void back() {
+  digitalWrite(input1,LOW);
+  digitalWrite(input2,HIGH);
+  digitalWrite(input3,LOW);
+  digitalWrite(input4,HIGH);
+ 
+}
+
+void left() {
+  digitalWrite(input1,HIGH);
+  digitalWrite(input2,LOW);
+  digitalWrite(input3,LOW);
+  digitalWrite(input4,LOW);
+  
+}
+
+void right() {
+  digitalWrite(input1,LOW);
+  digitalWrite(input2,LOW);
+  digitalWrite(input3,HIGH);
+  digitalWrite(input4,LOW);
+  
 }
 
